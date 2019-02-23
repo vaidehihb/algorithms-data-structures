7118578f6949b925e058babed300964f295b79e6
Vaidehi Bapat <vaidehihb@gmail.com>
2019-02-23 17:36:53 -0500

    Array Intersection main removed mergesort impl added

--- src/com/algo/ds/array/ArrayIntersection.java
+++ src/com/algo/ds/array/ArrayIntersection.java
@@ -7,41 +7,41 @@ import com.algo.ds.sorting_searching.binarysearch.template1.*;
 
 public class ArrayIntersection {
     // Using Hashset
-    public static int[] findIntersection(int[] nums1, int[] nums2){
-        if(nums1 == null || nums1.length == 0) return nums1;
-        if(nums2 == null || nums2.length == 0) return nums2;
+    public static int[] findIntersection(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums1.length == 0) return nums1;
+        if (nums2 == null || nums2.length == 0) return nums2;
         Set<Integer> set = new HashSet<>();
         Set<Integer> intersection = new HashSet<>();
 
-        for (int i: nums1){
+        for (int i : nums1) {
             set.add(i);
         }
 
-        for (int i:nums2){
-            if(set.contains(i)){
+        for (int i : nums2) {
+            if (set.contains(i)) {
                 intersection.add(i);
             }
         }
 
         int[] result = new int[intersection.size()];
         int j = 0;
-        for(int i:intersection){
+        for (int i : intersection) {
             result[j++] = i;
         }
         return result;
     }
 
     // using sorting and two pointers
-    public static int[] findIntersection1(int[] nums1, int[] nums2){
-        if(nums1 == null || nums1.length == 0) return nums1;
-        if(nums2 == null || nums2.length == 0) return nums2;
+    public static int[] findIntersection1(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums1.length == 0) return nums1;
+        if (nums2 == null || nums2.length == 0) return nums2;
         Arrays.sort(nums1);
         Arrays.sort(nums2);
         int i = 0, j = 0;
         Set<Integer> intersection = new HashSet<>();
-        while(i < nums1.length && j < nums2.length){
-            if(nums1[i] < nums2[j]) i++;
-            else if(nums1[i] > nums2[j]) j++;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) i++;
+            else if (nums1[i] > nums2[j]) j++;
             else {
                 intersection.add(nums1[i]);
                 i++;
@@ -50,66 +50,60 @@ public class ArrayIntersection {
         }
         int[] result = new int[intersection.size()];
         i = 0;
-        for(int k : intersection){
+        for (int k : intersection) {
             result[i++] = k;
         }
         return result;
     }
 
     // using sort and binary search
-    public static int[] findIntersection2(int[] nums1, int[] nums2){
-        if(nums1 == null || nums1.length == 0) return nums1;
-        if(nums2 == null || nums2.length == 0) return nums2;
-        if(nums2.length > nums1.length) return findIntersection2(nums2, nums1);
+    public static int[] findIntersection2(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums1.length == 0) return nums1;
+        if (nums2 == null || nums2.length == 0) return nums2;
+        if (nums2.length > nums1.length) return findIntersection2(nums2, nums1);
         Set<Integer> intersection = new HashSet<>();
 
         Arrays.sort(nums2);
-        for(int num: nums1)
-            if(BinarySearch.binarySearch(nums2, num) > -1) intersection.add(num);
+        for (int num : nums1)
+            if (BinarySearch.binarySearch(nums2, num) > -1) intersection.add(num);
 
         int[] result = new int[intersection.size()];
         int j = 0;
-        for (int i: intersection) {
+        for (int i : intersection) {
             result[j++] = i;
         }
         return result;
     }
 
     //using boolean array
-    public static int[] findIntersection3(int[] nums1, int[] nums2){
-        if(nums1 == null || nums1.length == 0) return nums1;
-        if(nums2 == null || nums2.length == 0) return nums2;
+    public static int[] findIntersection3(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums1.length == 0) return nums1;
+        if (nums2 == null || nums2.length == 0) return nums2;
         int max = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
 
-        for(int i: nums1){
+        for (int i : nums1) {
             min = Math.min(min, i);
             max = Math.max(max, i);
         }
 
-        boolean[] exists = new boolean[max-min+1];
+        boolean[] exists = new boolean[max - min + 1];
 
-        for(int i:nums1){
-            exists[i-min] = true;
+        for (int i : nums1) {
+            exists[i - min] = true;
         }
 
         int[] reault = new int[nums2.length];
         int idx = 0;
 
-        for (int i: nums2){
-            if(i >= min && i <= max && exists[i-min]) {
+        for (int i : nums2) {
+            if (i >= min && i <= max && exists[i - min]) {
                 reault[idx++] = i;
-                exists[i-min] = false;
+                exists[i - min] = false;
             }
         }
 
         return Arrays.copyOf(reault, idx);
 
-    }
-
-    public static void main(String[] args) {
-        int[] res = findIntersection3(new int[]{5,6,6,7,8,8,9}, new int[]{5,6,9});
-        for (int i:res)
-            System.out.println(i);
     }
 }
--- src/com/algo/ds/sorting_searching/sortingalgorithms/MergeSort.java
+++ src/com/algo/ds/sorting_searching/sortingalgorithms/MergeSort.java
@@ -1,4 +1,49 @@
 package com.algo.ds.sorting_searching.sortingalgorithms;
 
+import java.util.Arrays;
+
 public class MergeSort {
+    public static void mergeSort(int[] arr){
+        if(arr == null || arr.length <= 1) return;
+        mergeSort(arr, 0, arr.length - 1);
+    }
+
+    public static void mergeSort(int[] arr, int l, int r){
+        if(l>=r) return;
+        int m = l + (r-l)/2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m+1, r);
+        merge(arr, l, m, r);
+    }
+
+    private static void merge(int[] arr, int l, int m, int r){
+        int n1 = m-l+1;
+        int n2 = r - m;
+
+        int[] left = new int[n1];
+        int[] right = new int[n2];
+
+        for(int i = 0; i<n1; i++){
+            left[i] = arr[l+i];
+        }
+        for(int i = 0; i<n2; i++){
+            right[i] = arr[m+1+i];
+        }
+        int i = 0, j = 0, k = l;
+        while(i < n1 && j < n2){
+            if(left[i] < right[j]) {
+                arr[k] = left[i++];
+            }
+            else {
+                arr[k] = right[j++];
+            }
+            k++;
+        }
+        while(i < n1){
+            arr[k++] = left[i++];
+        }
+        while(j < n2){
+            arr[k++] = right[j++];
+        }
+    }
 }
