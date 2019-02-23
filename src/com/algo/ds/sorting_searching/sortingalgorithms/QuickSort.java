1cfe2a9c64bf3aa601b159bcc71c05acfe6ae456
Vaidehi Bapat <vaidehihb@gmail.com>
2019-02-23 18:05:47 -0500

    added quicksort impl

--- src/com/algo/ds/sorting_searching/sortingalgorithms/QuickSort.java
+++ src/com/algo/ds/sorting_searching/sortingalgorithms/QuickSort.java
@@ -1,4 +1,34 @@
 package com.algo.ds.sorting_searching.sortingalgorithms;
 
 public class QuickSort {
+    public static void quickSort(int[] arr){
+        if(arr == null || arr.length <= 1) return;
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    public static void quickSort(int[] arr, int low, int high){
+        if(low >= high) return;
+        int pivot = partition(arr, low, high);
+        quickSort(arr, low, pivot-1);
+        quickSort(arr, pivot+1, high);
+    }
+    public static int partition(int[] arr, int low, int high){
+        int pivot = arr[high];
+        int i = low - 1;
+        for(int j = low; j<high; j++){
+            if(arr[j] <= pivot){
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i+1, high);
+        return i+1;
+    }
+
+    private static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
 }
