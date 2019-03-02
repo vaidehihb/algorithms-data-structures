32e08a90802d34ec4a8dc9ed499bcfb7c4bda7e8
Vaidehi Bapat <vaidehihb@gmail.com>
2019-03-02 18:21:18 -0500

    - Buy and sell stocks DP

---
+++ src/com/algo/ds/dynamic_programming/BuyAndSellStocks.java
@@ -0,0 +1,18 @@
+package com.algo.ds.dynamic_programming;
+
+public class BuyAndSellStocks {
+    public static int maxProfit(int[] prices){
+        if(prices == null || prices.length == 0) return 0;
+        int maxProfit = 0, localProfit = 0;
+
+        for(int i=0; i<prices.length-1; i++){
+            localProfit = Math.max(0, localProfit+prices[i+1]-prices[i]);
+            maxProfit = Math.max(localProfit, maxProfit);
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{7,6,4,3,1}));
+    }
+}
