0649fe5ec809a705fb1c16403111e70b4242366b
Vaidehi Bapat <vaidehihb@gmail.com>
2019-03-21 22:55:09 -0400

    - Singleton design pattern

---
+++ src/com/java/design_patterns/Singleton.java
@@ -0,0 +1,18 @@
+package com.java.design_patterns;
+
+public class Singleton {
+    private static Singleton instance;
+    private Singleton(){
+
+    }
+    public static Singleton getInstance(){
+        if(instance == null){
+            synchronized (Singleton.class){
+                if(instance == null){
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+}
