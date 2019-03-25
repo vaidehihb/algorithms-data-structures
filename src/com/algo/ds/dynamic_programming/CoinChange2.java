23272c170732bc12d4b374817f86a3feb8435f12
Vaidehi Bapat <vaidehihb@gmail.com>
2019-03-25 09:35:35 -0400

    - Number of ways to make change

---
+++ src/com/algo/ds/dynamic_programming/CoinChange2.java
@@ -0,0 +1,16 @@
+package com.algo.ds.dynamic_programming;
+
+import java.util.Arrays;
+
+// number of ways
+public class CoinChange2 {
+    public int change(int[] coins, int amount){
+        if(coins == null || coins.length == 0 || amount == 0) return 0;
+        int[] ways = new int[amount + 1];
+        ways[0] = 1;
+        for(int coin:coins) {
+            for (int i=coin; i<= amount; i++) ways[i] += ways[i-coin];
+        }
+        return ways[amount];
+    }
+}
